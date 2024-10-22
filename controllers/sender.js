e9ea15cf4e8e9771a01b31d2dbb40fce9bc162b2
Damian Oguche <damianguch@yahoo.com>
2024-10-22 09:38:57 +0000

    Created the sender and traveller models controllers

---
+++ controllers/sender.js
@@ -0,0 +1,10 @@
+// POST: create request delivery
+const RequestDelivery = async (req, res) => {};
+
+// PUT: update shipping details
+const UpdateShippingDetails = async (req, res) => {};
+
+module.exports = {
+  UpdateShippingDetails,
+  RequestDelivery
+};
---
+++ controllers/traveller.js
@@ -0,0 +1,10 @@
+// POST: upload travel details
+const TravelDetails = async (req, res) => {};
+
+// PUT: edit travel details
+const UpdateTravelDetails = async (req, res) => {};
+
+module.exports = {
+  TravelDetails,
+  UpdateTravelDetails
+};
